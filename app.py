02f2348fcb2d1ab39102829dd6187f144a5dd550
Antonios Kritsas <ankrits@teiemt.gr>
2019-10-18 04:03:51 +0300

    refactoring the code

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask,jsonify,request,make_response
 from flask_cors import CORS
 import json
-
 app = Flask(__name__)
-
 CORS(app)
 
 # Returning the 
 @app.route('/',methods=['POST'])
 def indexing():
     return jsonify("yolo")
+    
+
